b1347ae667b7789582febd70dd24c62aa94c296b
Oussama Meqqadmi <oussama.meq@gmail.com>
2025-09-04 00:43:46 +0100

    Added some routes and built login, navbar, sidebar, footer design for the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@/components/ui/button"
+import { RouterProvider } from "react-router-dom";
+import router from "@/router/Index.jsx";
 
 function App() {
  
+  // adding routes to routes provided by router
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">    Hello world!  </h1>
+      <RouterProvider router={router} />
+
     </>
   )
 }
