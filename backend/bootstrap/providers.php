a3692bc988f77ea63a02c2ea66cd7e90fadfbc98
Oussama Meqqadmi <oussama.meq@gmail.com>
2025-09-30 15:02:03 +0100

    telescope comment out locally

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -2,6 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+   // App\Providers\TelescopeServiceProvider::class,
     Rebing\GraphQL\GraphQLServiceProvider::class,
 ];
